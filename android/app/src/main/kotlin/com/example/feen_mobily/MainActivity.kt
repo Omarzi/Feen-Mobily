ee3c9fee1edaeff2d8ba66f49937670818c9d51e
omar <omar@gmail.com>
2022-12-24 21:26:20 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/feen_mobily/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.feen_mobily
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:feen_mobily/app/my_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
