20d5ca5a86d4715cc4caee98ec64128a104bf67d
Omar <mm5094397@gmail.com>
2023-01-28 23:26:29 +0200

    complete the strucure

---
+++ lib/data/data_provider/local/my_cache_keys.dart
@@ -0,0 +1,17 @@
+enum MyCacheKeys {
+  isOnBoardingViewed,
+  // myId,
+  // myEmail,
+  // myPassword,
+  // myUserName,
+  // roles,
+  // selectedColorIndex,
+  token,
+  saveLang,
+  saveLanguage,
+  // myPhone,
+  // galary,
+  // camera,
+  // activeCart,
+  // isLove
+}
